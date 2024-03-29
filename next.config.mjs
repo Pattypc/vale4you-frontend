f2e1a06913ca3d9837f0296958656e64f0d48ab5
mateusp7 <mateusdev1337@gmail.com>
2024-03-29 14:48:54 -0300

    fix: update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  trailingSlash: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**'
+      }
+    ]
+  }
+}
 
-export default nextConfig;
+module.exports = nextConfig
